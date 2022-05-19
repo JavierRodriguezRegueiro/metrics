2f5ba708168bb6eebce8347295402520b0662db6
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2022-05-19 17:55:29 +0200

    Remove unused imports

--- src/main/java/com/factorial/metrics/web/frontend/src/App.jsx
+++ src/main/java/com/factorial/metrics/web/frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import PutMetric from './api/PutMetric';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import GetMetricsInRange from './api/GetMetricsInRange';
 import PutMetricForm from "./components/PutMetricForm/PutMetricForm";
 import GetMetrics from "./components/GetMetrics/GetMetrics";
 import './App.css';
